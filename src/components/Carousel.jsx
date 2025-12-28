888a6255124252e44f6ba84d71336b5ed8c6d5a7
jdavid1991 <jrrdriwtf@gmail.com>
2025-12-28 11:25:10 -0800

    se realizan ajustes de imagenes para mejor UX

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,8 +9,8 @@ import aws from "/assets/aws.png";
 import backend from "/assets/backend.png";
 import fullstack from "/assets/fullstack.png";
 import java from "/assets/java.png";
-import linux from "/assets/linux.png";
-import javascript from "/assets/javascript.png";
+import linux from "/assets/linux.webp";
+import javascript from "/assets/javascript.webp";
 
 export default function Carousel() {
   const slides = [aws, backend, fullstack, java, linux, javascript];
