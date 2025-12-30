c8189742cb3700f9132c5e48c6181a4db513e393
jdavid1991 <jrrdriwtf@gmail.com>
2025-12-30 12:11:08 -0800

    Se realizan ajustes de UI y UX

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <div className="bg-[#0F172A] ">
         <div>
           <p className="text-center sm:pt-25 pt-15 pb-4 text-slate-300 text-sm font-semibold">
-            © 2025 - {year} Portafolio elaborado por Juan David R
+            © 2024 - {year} Portafolio elaborado por Juan David R
           </p>
         </div>
       </div>
