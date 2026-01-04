8f2b7f9a10f258e0de2af547352a66b0450acd5d
jdavid1991 <jrrdriwtf@gmail.com>
2026-01-03 18:08:46 -0800

    Se realiza ajustes varios en UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,8 @@ export const Footer = () => {
         <div className="pt-15">
           <div className=" container mx-auto  border  border-slate-800"></div>
           <p className="text-center sm:pt-20 pt-12 pb-4 text-slate-300 text-sm">
-            © 2024 - {year} Portafolio elaborado por Juan David R. <br /> Todos los derechos reservados.
+            Portafolio elaborado por Juan David R. <br /> © 2024 - {year} Todos
+            los derechos reservados.
           </p>
         </div>
       </div>
